f892a90dedf04edf1635b7d45e1fd87ce5d1917f
Felipe mac <fconcha.almeida@gmail.com>
2020-01-03 13:01:28 -0300

    feat(staff): add network layer

---
+++ api/staff/network.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const response = require('../responses');
+const router = express.Router();
+const controller = require('./controller');
+
+router.get('/', async function (req, res) {
+  try {
+    const staff = await controller.getStaff();      
+    response.succes(req, res, staff);
+  }
+  catch (error) {
+    console.error(error);
+    response.error(req, res, 'Unexpected Error', 500, error);
+  }
+}); 
