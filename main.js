53283de5f4e37b79b6f83dc88ed0361dc919341f
Felipe mac <fconcha.almeida@gmail.com>
2020-01-03 12:32:58 -0300

    feat(server): api routing implementation

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const api = require('./api/router');
 
 var app = express();
 const port = 4040;
 
 app.use(bodyParser);
+api.routing(app);
 app.listen(port, () => console.log(`Server listen on ${port}`));
