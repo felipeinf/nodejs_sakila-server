6b707726ab498dcc9e9ab02f7301d6f700f9a47a
Felipe mac <fconcha.almeida@gmail.com>
2020-01-05 15:40:58 -0300

    docs(server): documentation and license aggregation

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const api = require('./api/router');
+/**
+ * Sakila Server 
+ * 
+ * @description REST API server developed using the NodeJs, Express and MySql. 
+ * @version 1.0
+ * @author Felipe Concha Almeida
+ * @since January 2020
+ * @license Apache
+ */
 
-var app = express();
-const port = 4040;
-
-app.use(bodyParser.json());
-api.routing(app);
-app.listen(port, () => console.log(`Server listen on ${port}`));
+(function main(){ 
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const api = require('./api/router');
+  
+  const app = express();
+  let port = 3000;
+  
+  app.use(bodyParser.json());
+  api.routing(app);
+  app.listen(port, () => console.log(`Server listen on ${port}`));
+})();
