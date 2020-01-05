b51445c8b0ae463ede993c4f9ed9ff2af427f35f
Felipe mac <fconcha.almeida@gmail.com>
2020-01-05 12:27:14 -0300

    feat(api): creation of routes, shop, address, city and country

--- api/router.js
+++ api/router.js
@@ -1,7 +1,15 @@
 const staff = require('./staff/network');
+const city = require('./city/network');
+const country = require('./country/network');
+const address = require('./address/network');
+const store = require('./store/network');
 
 function routing(server) {
   server.use('/staff', staff);
+  server.use('/store', store);
+  server.use('/address', address);
+  server.use('/city', city);
+  server.use('/country', country);
 }
 
 module.exports = {
