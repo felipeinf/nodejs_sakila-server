1780d5edaa49407065fd0c82406686ce7d6dd8ca
Felipe mac <fconcha.almeida@gmail.com>
2020-01-04 21:34:40 -0300

    refactor(api): add args in mysql setdatabase

--- api/mysql.js
+++ api/mysql.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql');
 const util = require('util');
-const connectParams = require('./environments/sakila-params')
+const connectParams = require('./_environments/sakila-params');
 
 function setDatabase(){
-  var connection = mysql.createConnection(connectParams);
+  let connection = mysql.createConnection(connectParams);
 
   return {
-    query(sql){
+    query(sql, values){
       return util
         .promisify(connection.query)
-        .call(connection, sql);
+        .call(connection, sql, [values]);
     },
     disconnect(){
       return util
