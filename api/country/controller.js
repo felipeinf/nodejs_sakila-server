cfbf367fb101500979b872c072a4da3fc6f0b181
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-03-02 11:10:21 -0300

    feat(country): getCountries

--- api/country/controller.js
+++ api/country/controller.js
@@ -1,7 +1,15 @@
 const repository = require('./repository');
+const cityController = require('../city/controller');
 
-function getCountries(){
-  return repository.getCountries();
+async function getCountries(){
+  let countries = await repository.getCountries();
+  let cities = await cityController.getCities();
+
+  for (let i = 0; i < countries.length; i++) {    
+    countries[i].cities = cities.filter((city) => city.country_id == countries[i].country_id);
+  }
+
+  return countries;
 }
 
 module.exports = {
